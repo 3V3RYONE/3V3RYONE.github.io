a08977129cc237d60dd3c4cb7ba827726cd016b4
3V3RYONE <beleswarprasad@gmail.com>
2021-01-05 01:37:59 +0530

    Initialize blog setup

---
+++ content/pages/About.md
@@ -0,0 +1,15 @@
+Title: About
+Author: Beleswar
+Date: 12-22-2020
+
+##If you don't realize a **Déjà vu**, you have to realize you are creating a **Déjà vu**!    
+
+
+Hii there!  
+I'm Beleswar. I am an undergrad CS student who loves to code and create.  
+I like to explore new technologies. Fond of quantum mechanics, mysteries, paradox and time.
+This blog recites my thoughts, findings and techs.  
+
+You can reach me below.  
+
+Thanks for stopping by! :)
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Beleswar'
+SITENAME = 'INSIGHT'
+SITEURL = 'https://3v3ryone.github.io/Blog'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = "theme/pelican-clean-blog"
+HEADER_COLOR = "black"
+COLOR_SCHEME_CSS = "github.css"
+SOCIAL = (('github','https://www.github.com/3V3RYONE'),('linkedin','https://www.linkedin.com/in/beleswar-prasad-padhi-2416241bb/'),('envelope','mailto:beleswarprasad@gmail.com'))
+SHOW_SOCIAL_ON_INDEX_PAGE_HEADER = False
+
+SITESUBTITLE = 'Blog'
+SHOW_SITESUBTITLE_IN_HTML = True
+SHOW_CREDIT_BY_AUTHOR = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Blogroll
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
